ba73520dae25f4d541b2bcfb24e086e2d2d15e1d
friwid <50137120+friwid@users.noreply.github.com>
2024-01-25 17:39:36 +0100

    Add 05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+  let sum = 0;
+  let lowerNum;
+  let higherNum;
+  if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return "ERROR";
+  if (!(firstNum >= 0) ||  !(secondNum >= 0)) return "ERROR";
+  (firstNum <= secondNum ? 
+    (() => {lowerNum = firstNum; higherNum = secondNum;})() : 
+    (() => {lowerNum = secondNum; higherNum = firstNum;})()
+  );
 
+  for (;lowerNum <= higherNum;lowerNum++ ) {
+    sum += lowerNum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
