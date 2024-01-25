73fd1a5bf8a8628ba2ec052787a05672febbe6a8
friwid <50137120+friwid@users.noreply.github.com>
2024-01-25 14:01:49 +0100

    Add 03_reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+let stringArray = string;
 
+stringArray = stringArray.split("").reverse().join("");
+
+return stringArray;
 };
 
 // Do not edit below this line
