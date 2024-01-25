3612cfbb199bef659143ccd2d8d41d0bf3b277b4
friwid <50137120+friwid@users.noreply.github.com>
2024-01-25 15:16:02 +0100

    Add 04_removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array = [], ...values) {
+  let newArray = [];
+  let uniqueValue; 
 
+  for (let arr of array) {
+    uniqueValue = true;
+    for (let val of values) {
+      if (arr === val) {
+        uniqueValue = false
+        break;
+      };
+
+    }
+    if (uniqueValue) {
+      newArray.push(arr);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
