9320a58ac2cf88efe239072dec4724065080338c
friwid <50137120+friwid@users.noreply.github.com>
2024-01-26 16:15:19 +0100

    Add 09_palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const array = Array.from(string.toLowerCase().replace(/[^a-zA-Z1-9]/g, ""));
+  let palindrome = true;
+  for (let i = 0; i < ((array.length)/2); i++) {
+    if (array[i] !== array[array.length-1-i]) { 
+      palindrome = false;
+      break;
+    }
+  }
+  return palindrome;
 };
 
 // Do not edit below this line
