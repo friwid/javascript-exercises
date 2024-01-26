cba54ee30287e544cdbf18e4859ab8dfc107643f
friwid <50137120+friwid@users.noreply.github.com>
2024-01-26 15:33:31 +0100

    Add 08_calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => {
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total, num) => {
+    return total * num;
+  });
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
