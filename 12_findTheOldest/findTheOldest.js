42716186b9e2da808a8f2ec9e7b7dfb398f11c37
friwid <50137120+friwid@users.noreply.github.com>
2024-01-26 19:34:51 +0100

    Add 12_findTheOldes solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array.reduce(function(oldest, currentPerson) {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    if (oldestAge < currentAge) {
+      return currentPerson;
+    } else {
+      return oldest;
+    }
+  });
+};
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
