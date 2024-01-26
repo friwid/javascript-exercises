c0f003266dbe01dfd0a40be7c20abec7f5e14a44
friwid <50137120+friwid@users.noreply.github.com>
2024-01-26 16:55:28 +0100

    Add 10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let counter;
+  let previousNum1 = 0;
+  let previousNum2 = 1;
+  if (typeof num !== "number") {
+    counter = parseInt(num)
+  } else {
+    counter = num;
+  }
+
+  if (counter < 0) return "OOPS";
+  if (counter == 0) return 0;
+
+  for (let i = 2; i <= counter; i++) {
+    let sum = previousNum2 + previousNum1;
+    previousNum1 = previousNum2;
+    previousNum2 = sum;
+  }
+
+  return previousNum2;
 
 };
 
